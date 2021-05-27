581a9372c1ee80c61617af510c6cde60abd50c4b
AdrienPinheiro <adrienpnrpro@gmail.com>
2021-05-27 15:44:23 +0200

    api pour page accueil use manque: mise en page et responsive

---
+++ js/api.js
@@ -0,0 +1,62 @@
+let cardArticles = null;
+let dataTeddies = null;
+
+
+function updateTeddies(){
+    fetch('http://localhost:3000/api/teddies')
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data", data);
+            dataTeddies = data;    
+            drawTeddies(data);    
+     })
+    .catch(function(error){
+        console.error(error);
+    }) 
+}
+
+function drawTeddies(teddies) {
+    teddies.forEach(function (teddy) {
+        let cardTeddy = document.createElement("div");
+        cardTeddy.setAttribute("class", "card-article");
+
+        let cardLink = document.createElement("a");
+        cardLink.setAttribute("href", "/teddy/" + teddy._id);
+        cardTeddy.append(cardLink);
+
+        let cardDescrition = document.createElement("div");
+        cardDescrition.setAttribute("class", "card-description");
+
+        let cardImg = document.createElement("img");
+        cardImg.setAttribute("src", teddy.imageUrl);
+        cardImg.setAttribute("class", "card-img");
+        cardLink.append(cardImg, cardDescrition);
+
+        let cardName = document.createElement("h3");
+        cardName.setAttribute("class", "card-name");
+        cardName.innerHTML = teddy.name;
+
+        let cardPrice = document.createElement("p")
+        cardPrice.setAttribute("class", "card-price");
+        cardPrice.innerHTML = `Prix :${teddy.price}`;
+
+        let cardColor = document.createElement("p");
+        cardColor.setAttribute("class", "card-color");
+        cardColor.innerHTML = `Différent coloris:`;
+        cardDescrition.append(cardName, cardPrice, cardColor);
+
+        const teddyColors = teddy.colors;
+
+        for(i = 0; i < teddyColors.length; i++){
+        let teddyColor = document.createElement("div");
+        teddyColor.setAttribute("class", teddyColors[i]);
+        cardColor.append(teddyColor);}
+                
+        cardArticles.append(cardTeddy);
+    })
+}
+
+window.addEventListener("DOMContentLoaded", function (){
+    cardArticles = document.querySelector('.card-articles')
+    updateTeddies();
+},false);
---
+++ js/index.js
@@ -0,0 +1,5 @@
+const search = document.getElementById('search-input');
+
+search.addEventListener('input', (e) =>{
+    console.log(e.target.value);
+})
