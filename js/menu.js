589ced877d4ca90b0371a76893b5160b36208f77
AdrienPinheiro <adrienpnrpro@gmail.com>
2021-06-02 12:26:23 +0200

    W3C css et html validé Filtres/Catégorie marche A faire: animation filtres/catégories rajouter des éléments dans filtres/catégories rajouter l'option cliquable Refaire le site plus jolie

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,13 @@
 window.addEventListener("DOMContentLoaded", function (){
     const bouton = document.querySelector(".research-category")
-    const appear = document.querySelector(".menu-btn")
-    const menu = document.querySelector(".menu")
     const column = document.querySelector(".column")
     const category = document.querySelector(".category")
     const filtres = document.querySelector(".fitres")
 
     bouton.addEventListener('click', () =>{
         console.log("Youpi", document);
-        appear = document.classList.add('menu-btn-appear');
-        menu = document.classList.add('menu-background');
+        appear = document.querySelector(".menu-btn").classList.add('menu-btn-appear');
+        menu = document.querySelector(".menu").classList.add('menu-background');
     });
 },false);
 
